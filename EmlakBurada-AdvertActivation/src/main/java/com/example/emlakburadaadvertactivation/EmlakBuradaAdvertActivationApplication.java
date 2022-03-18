41ec815fd64fb6eb10c4df99a7b3a3ec12c31780
TacettinUtkuSuer <tacettinutkusuer@gmail.com>
2022-03-18 14:20:08 +0300

    EmlakBurada Advert Activation service created.

---
+++ EmlakBurada-AdvertActivation/src/main/java/com/example/emlakburadaadvertactivation/EmlakBuradaAdvertActivationApplication.java
@@ -0,0 +1,13 @@
+package com.example.emlakburadaadvertactivation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmlakBuradaAdvertActivationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmlakBuradaAdvertActivationApplication.class, args);
+    }
+
+}
