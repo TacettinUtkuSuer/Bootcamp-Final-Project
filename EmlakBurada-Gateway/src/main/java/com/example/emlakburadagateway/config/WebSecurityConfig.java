525f68f0de1e40e61145ec28712bb20335084743
TacettinUtkuSuer <tacettinutkusuer@gmail.com>
2022-03-19 15:59:20 +0300

    Gateway is ready for use.

---
+++ EmlakBurada-Gateway/src/main/java/com/example/emlakburadagateway/config/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.example.emlakburadagateway.config;
+
+import com.example.emlakburadagateway.filter.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class WebSecurityConfig{
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        //// @formatter:off
+        http
+                .httpBasic().disable()
+                .formLogin().disable()
+                .csrf().disable();
+
+        return http.build();
+        // @formatter:on
+
+    }
+
+    @Autowired
+    JwtFilter filter;
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                //// @formatter:off
+
+
+                .route("auth",
+                        r -> r.path("/auth/**")
+                                .uri("http://localhost:9093"))
+                .route("emlakburada",
+                        r -> r.method(HttpMethod.POST)
+                                .and()
+                                .path(("/adverts/**"))
+                                .filters(f -> f.filter(filter)).uri("http://localhost:9090"))
+                .route("emlakburada",
+                        r -> r.method(HttpMethod.GET)
+                                .and()
+                                .path("/hello/**")
+                                .filters(f -> f.filter(filter)).uri("http://localhost:9090"))
+                .route("emlakburada-banner",
+                        r -> r.path("/banners/**")
+                                .filters(f -> f.filter(filter)).uri("http://localhost:8081"))
+                .build();
+
+        // @formatter:on
+    }
+
+}
